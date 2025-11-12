3e44fc34b6cc0ed716e29423e8f34a51288c153f
David-Horjet <davidojetola956@gmail.com>
2025-11-12 06:29:54 +0100

    chore: working on claiming of reward

--- src/lib/api/predictions.ts
+++ src/lib/api/predictions.ts
@@ -14,7 +14,6 @@ export interface CreatePredictionResponse {
     id: string
     pool_id: string
     user_wallet: string
-    predicted_price: number
     amount: number
     reward: number | null
     status: string
